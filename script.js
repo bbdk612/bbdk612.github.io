68f55eb768c0ef80726ebe532a0316228c327a50
bbdk612 <smsmisha51@gmail.com>
2019-08-24 08:04:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ $("div.start").on('click', function() {
 			} else if (t1 == t2) {
 				$('img.first.car').animate({left:  (widthField1 - (+$('img.first.car').width() + 10)) + 'px'}, t1 * 100);
 				$('img.second.car').animate({left:  (widthField1 - (+$('img.second.car').width() + 10)) + 'px'}, t2 * 100, function(){			
-				$('img.car').animate({right: (widthField1 - (+$('img.second.car').width() + 10)) + 'px'}, 1000);
+				$('img.car').css({left: -(widthField1 +'px'}, 1000);
 				setTimeout(opendialog3(), 1000);
 				
 			});
